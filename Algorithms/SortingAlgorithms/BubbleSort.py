2320b8ca83ba2cd9f87eb5ac81782f971c0e682d
ayushgupta-repo <ayushgupta.8848@gmail.com>
2022-06-28 18:23:45 +0530

    Created Algorithm directory and also created Bubble Sort algorithm in SortingAlgorithm directory

---
+++ Algorithms/SortingAlgorithms/BubbleSort.py
@@ -0,0 +1,30 @@
+# Defining Bubble Sort: It is the sorting algorithm in which repeatedly two adjancet pairs are compared and if they are in wrong order then they get swapped
+
+# def bubbleSort(customList):
+#     for i in range(len(customList)-1):
+#         for j in range(len(customList)-i-1):
+#             if customList[j] > customList[j+1]:
+#                 # do swapping
+#                 customList[j], customList[j+1] = customList[j+1], customList[j]
+    
+#     print(customList)
+
+# To make it more optimized
+def bubbleSort(customList):
+    found = False
+
+    for i in range(len(customList)-1):
+        # As after each sorting phase last element reaches its position so for further phases we don't need to compare other elements with it so extra -i is given in the below condition
+        for j in range(len(customList)-i-1):
+            if customList[j] > customList[j+1]:
+                # do swapping
+                customList[j], customList[j+1] = customList[j+1], customList[j]
+                found = True
+
+        if not found:
+            break
+    
+    print(customList)
+
+cList = [6, 3, 8, 9, 1]
+bubbleSort(cList)
